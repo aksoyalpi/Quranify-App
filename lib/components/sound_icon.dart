de0cac073a6dc6acae9af78765cff010ecb5f934
aksoyalpi <a.aksoy@gmail.com>
2024-07-14 20:19:51 +0200

    box für das auswählen eines natur sounds funktioniert nun (mit Overlay)

--- lib/components/sound_icon.dart
+++ lib/components/sound_icon.dart
@@ -12,6 +12,65 @@ class SoundIcon extends StatefulWidget {
 class _SoundIconState extends State<SoundIcon>
     with SingleTickerProviderStateMixin {
   bool _isExpanded = false;
+  OverlayEntry? _overlayEntry;
+  final GlobalKey _iconKey = GlobalKey();
+
+  void _toggleOverlay(BuildContext context, SurahsProvider value) {
+    if (_isExpanded) {
+      _overlayEntry?.remove();
+      _overlayEntry = _createOverlayEntry(context, value);
+      Overlay.of(context).insert(_overlayEntry!);
+    } else {
+      _overlayEntry?.remove();
+      _overlayEntry = null;
+    }
+  }
+
+  @override
+  void dispose() {
+    _overlayEntry?.remove();
+    super.dispose();
+  }
+
+  OverlayEntry _createOverlayEntry(BuildContext context, SurahsProvider value) {
+    RenderBox renderBox =
+        _iconKey.currentContext!.findRenderObject() as RenderBox;
+    Offset position = renderBox.localToGlobal(Offset.zero);
+    double iconWidth = renderBox.size.width;
+    double iconHeight = renderBox.size.height;
+
+    return OverlayEntry(
+      builder: (context) => Positioned(
+        top: position.dy - (iconHeight + 20),
+        left: position.dx - 65,
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          width: _isExpanded ? 150 : 0,
+          height: _isExpanded ? 40 : 0,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Theme.of(context).colorScheme.secondary),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: List.generate(
+              value.soundIconDatas.length,
+              (index) => GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isExpanded = false;
+                    });
+                    value.soundIndex = index;
+                    _overlayEntry?.remove();
+                    _overlayEntry = null;
+                  },
+                  child: Icon(value.soundIconDatas[index])),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,34 +78,16 @@ class _SoundIconState extends State<SoundIcon>
       builder: (context, value, child) => Column(
         children: [
           // animated container for selecting nature sound
-          Center(
-            child: AnimatedContainer(
-              duration: Durations.short4,
-              curve: Curves.bounceIn,
-              width: _isExpanded ? 150 : 0,
-              height: _isExpanded ? 40 : 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: List.generate(
-                  value.soundIconDatas.length,
-                  (index) => GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _isExpanded = false;
-                        });
-                        value.soundIndex = index;
-                      },
-                      child: Icon(value.soundIconDatas[index])),
-                ),
-              ),
-            ),
-          ),
 
           // Icon showing current nature sound
           GestureDetector(
-              onTap: () => setState(() {
-                    _isExpanded = true;
-                  }),
+              key: _iconKey,
+              onTap: () {
+                setState(() {
+                  _isExpanded = !_isExpanded;
+                  _toggleOverlay(context, value);
+                });
+              },
               child: Icon(
                 value.soundIconData,
                 color: value.soundOn
